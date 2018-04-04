ae7bfb7d272850598a5cde46a4a5f50eb6918f5c
John Strong <jstrong@shutterstock.com>
2018-04-04 14:54:52 -0400

    init: add parameters for alternate gem sources
    I would like to be able to install the infoblox gem on servers in my
    environment that only have access to internal rubygems proxies. To do
    that in Puppet code, I need to be able to expose these two file resouce
    parameters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
 # Installs infoblox gem
-class infoblox {
+class infoblox (
+  Optional[String] $source = undef,
+  Optional[Array] $install_options = undef,
+) {
 
   package { 'infoblox':
-    ensure   => present,
-    provider => 'puppet_gem',
+    ensure          => present,
+    provider        => 'puppet_gem',
+    source          => $source,
+    install_options => $install_options,
   }
 
 }
